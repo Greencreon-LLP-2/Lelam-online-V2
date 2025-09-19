16ddde6446f52385066fa53de25e70a8c91ee506
MITHINVEL <151605349+MITHINVEL@users.noreply.github.com>
2025-09-19 21:12:00 +0530

    UI updation
    Notifications ui, card redesigned ,
    Faq page without breaking api, card ui redesigned

--- lib/feature/notification/view/pages/notification_page.dart
+++ lib/feature/notification/view/pages/notification_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lelamonline_flutter/core/theme/app_theme.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({super.key});
@@ -6,19 +7,56 @@ class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100], 
       appBar: AppBar(
         title: const Text('Notifications'),
         centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
+        foregroundColor: Colors.white,
+        backgroundColor: AppTheme.primaryColor,
+        elevation: 2,
       ),
       body: ListView.builder(
+        padding: const EdgeInsets.all(12),
         itemCount: 10,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Notification $index'),
-            subtitle: Text('Notification $index'),
-            leading: Icon(Icons.notifications),
+          return Card(
+            elevation: 3,
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: ListTile(
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 16,
+              ),
+              leading: CircleAvatar(
+                radius: 24,
+                backgroundColor: Colors.blue.shade100,
+                child: const Icon(
+                  Icons.notifications,
+                  color: Colors.blue,
+                ),
+              ),
+              title: Text(
+                'Notification ${index + 1}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                ' notification ${index + 1}',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                ),
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.arrow_forward_ios, size: 18),
+                onPressed: () {
+          
+                },
+              ),
+            ),
           );
         },
       ),
